3d8015657e9249fd0444d20573a46fe23358e7bb
mg901 <mgreen097@gmail.com>
2019-04-16 13:39:21 +0300

    initial commit :tada:

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["airbnb"],
+  rules: {
+    "react/jsx-closing-bracket-location": 0,
+    "import/no-default-export": 2,
+    "import/no-extraneous-dependencies": 0,
+    "import/prefer-default-export": 0,
+    "react/jsx-filename-extension": 0,
+    "react/jsx-wrap-multilines": 0,
+    "react/forbid-prop-types": 0
+  }
+};
