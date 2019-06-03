bd150764c171a8e943a5b4cd4d966d9f390c9571
mg901 <mgreen097@gmail.com>
2019-06-03 17:50:29 +0300

    chore(rules): add new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,25 @@
 module.exports = {
-  extends: ["airbnb"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    allowImportExportEverywhere: true
+  },
+  env: {
+    es6: true
+  },
+  extends: ["airbnb", "prettier"],
+  plugins: ["prettier", "prettier"],
   rules: {
-    "react/jsx-closing-bracket-location": 0,
+    "no-unused-vars": 1,
+    "newline-before-return": 2,
+    "arrow-body-style": 2,
     "import/no-default-export": 2,
-    "import/no-extraneous-dependencies": 0,
     "import/prefer-default-export": 0,
-    "react/jsx-filename-extension": 0,
+    "import/no-extraneous-dependencies": 0,
+    "react/jsx-indent-props": 0,
     "react/jsx-wrap-multilines": 0,
-    "react/forbid-prop-types": 0
+    "react/jsx-filename-extension": 0,
+    "react/jsx-closing-bracket-location": 0
   }
 };
